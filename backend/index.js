763a909d4ed0ea261ab2ba5d0cf29b9d7c7e250e
Afnan Mazhar <m.afnanmazhar@gmail.com>
2024-11-17 21:52:19 +0500

    final

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
-
 import cookieParser from "cookie-parser";
 import path from 'path'; // Correct import for the path module
 
-import authRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import { app, server } from "./socket/socket.js";
@@ -16,10 +15,11 @@ const __dirname = path.resolve(); // Fixed the capitalization
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(cookieParser());
 
-
+// CORS setup
 app.use(cors({
   origin: [
     'https://chat-app-3-0zzu.onrender.com',
@@ -30,32 +30,27 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-
-app.use(cors({ origin: 'https://chat-app42.netlify.app' }));
-
-
+// Routes
 app.get('/home', (req, res) => {
   res.send('Hello, World!');
 });
 
-// Use the /api/auth prefix for auth routes
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
-
-app.use(express.static(path.join(__dirname, "frontend", "dist"))); 
-
-
-app.use(express.static('frontend/dist'));
+// Serve static files
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve('frontend', 'dist', 'index.html'));
 });
 
-
-
 // Start the server
-server.listen(PORT, () => {
-  connectToMongoDB();
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  await connectToMongoDB();
+  server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+};
+
+startServer();
